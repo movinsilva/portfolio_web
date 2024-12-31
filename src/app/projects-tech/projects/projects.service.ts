7fe707145822899dee689fd5f7b40c89cbb7e859
Movin Silva <movinpinsara@gmail.com>
2024-12-31 14:37:12 +0530

    feat: add deploy script and update project details in ProjectsService

--- src/app/projects-tech/projects/projects.service.ts
+++ src/app/projects-tech/projects/projects.service.ts
@@ -1,67 +1,67 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProjectsService {
-
-  constructor() { }
+  constructor() {}
 
   getProjects() {
     return [
       {
-        title: "ReactJS NodeJS Express PostgreSQL",
-        name: "Travel GO",
-        description: "Train boking system with frontend, admin-panel, backend",
-        technology: "Javascript, CSS, HTML",
-        link: "/portfolio_web//#/travelgo",
-        applyAnimation: true,
-      },
-      {
-        title: "Angular",
-        name: "Portfolio Website",
-        description: "Portfolio website designed using angular, typescript",
-        technology: "Typescript, HTML, CSS",
-        link: "/portfolio_web/#/portfolio-web",
-        applyAnimation: true,
-      },
-      {
-        title: "HTML CSS JS",
-        name: "ECODE IT Website",
-        description: "Company profile website designed using HTML, CSS, JS",
-        technology: "HTML, CSS, Javascript",
-        link: "https://ecodeit.com",
+        title: 'JS TS React MUI',
+        name: 'Web SDKs',
+        description: 'A set of Javascript SDKs to embed authentication',
+        technology: 'Typescript, React, OAuth',
+        link: 'https://github.com/asgardeo/web-ui-sdks',
         applyAnimation: false,
-      }
-    ]
+      },
+      {
+        title: 'React Node Express PostgreSQL',
+        name: 'Travel GO',
+        description: 'Train boking system with frontend, admin-panel, backend',
+        technology: 'Javascript, React, Material UI, Node, Express, PostgreSQL',
+        link: '/portfolio_web//#/travelgo',
+        applyAnimation: false,
+      },
+      {
+        title: 'Angular',
+        name: 'Portfolio Website',
+        description: 'Portfolio website designed using angular, typescript',
+        technology: 'Typescript, HTML, CSS',
+        link: '/portfolio_web/#/portfolio-web',
+        applyAnimation: false,
+      },
+    ];
   }
 
   getProjects2() {
     return [
       {
-        title: ".Net-Core",
-        name: "HRMS",
-        description: "HRMS desktop application designed using .Net Core",
-        technology: "C#",
-        link: "/portfolio_web//#/hrms",
+        title: '.Net-Core',
+        name: 'HRMS',
+        description: 'HRMS desktop application designed using .Net Core',
+        technology: 'C#',
+        link: '/portfolio_web//#/hrms',
         applyAnimation: true,
       },
       {
-        title: "HTML CSS JS",
-        name: "IES Website",
-        description: "Company profile website designed using HTML, CSS, JS",
-        technology: "HTML, CSS, Javascript",
-        link: "https://ecodeit.com",
+        title: 'HTML CSS JS',
+        name: 'IES Website',
+        description: 'Company profile website designed using HTML, CSS, JS',
+        technology: 'HTML, CSS, Javascript',
+        link: 'https://www.ies.lk/',
         applyAnimation: false,
       },
       {
-        title: "Flutter",
-        name: "Money Manager App",
-        description: "Money management mobile app designed using flutter, SQFLite",
-        technology: "Dart",
-        link: "/portfolio_web/#/money-manager",
+        title: 'Flutter',
+        name: 'Money Manager App',
+        description:
+          'Money management mobile app designed using flutter, SQFLite',
+        technology: 'Dart',
+        link: '/portfolio_web/#/money-manager',
         applyAnimation: false,
-      }
-    ]
+      },
+    ];
   }
 }
