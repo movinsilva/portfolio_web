cbcd1af6f594661a7e2e59133b616191e606ea8a
Movin Silva <movinpinsara@gmail.com>
2025-09-21 10:55:54 +0530

    fix: update environment configuration; remove sensitive data files and enhance .gitignore rules

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  emailjs: {
-    serviceId: 'service_9296a8u',
-    templateId: 'template_j4qnjmr',
-    publicKey: 'OxxSXuQwvu4-rDNy5',
-  },
-};
